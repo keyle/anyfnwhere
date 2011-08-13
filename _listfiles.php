e889d7b1033ed64ce71004e3a51ba4a302a9a0da
keyle <keyle@capsule.org>
2011-08-14 00:23:46 +1000

    initial import

---
+++ _listfiles.php
@@ -0,0 +1,11 @@
+<?php
+$dir = "./";
+
+if (is_dir($dir)) {
+    if ($dh = opendir($dir)) {
+        while (($file = readdir($dh)) !== false) {
+            echo "$file <br/>";//: filetype: " . filetype($dir . $file) . "\n";
+        }
+        closedir($dh);
+    }
+}
---
+++ _savefile.php
@@ -0,0 +1,18 @@
+<?php
+
+if(!isset($_POST['targetfile']))
+	die('whoops, no file');
+
+if(!isset($_POST['content']))
+	die('whoops, no content');
+
+$stringData = $_POST['content'];
+$stringData = stripslashes($stringData);
+
+$myFile = "target.html";
+$fh = fopen($myFile, 'w') or die("whoops, can't open file");
+
+fwrite($fh, $stringData);
+fclose($fh);
+
+echo 7;
